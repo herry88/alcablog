a5c3cf7f78106c7c7ee7c1e21b81625572176aab
herry88 <herry081288@gmail.com>
2020-03-26 07:16:50 +0700

    AddMigrattionModifyMenu

---
+++ resources/views/layouts/menu.blade.php
@@ -0,0 +1,21 @@
+<ul class="menu js__accordion">
+    <li>
+        <a class="waves-effect" href="/categori"><i class="menu-icon fa fa-th-large"></i><span>Kategori</span></a>
+    </li>
+    <li>
+        <a class="waves-effect" href="/berita"><i class="menu-icon fas fa-newspaper"></i><span>Berita</span></a>
+        <!-- /.sub-menu js__content -->
+    </li>
+    <li>
+        <a class="waves-effect" href="/team"><i class="menu-icon fas fa-users"></i><span>Team</span></a>
+        <!-- /.sub-menu js__content -->
+    </li>
+    <li>
+        <a class="waves-effect" href="/portopolio"><i class="menu-icon fas fa-book-open"></i><span>Portopolio</span></a>
+        <!-- /.sub-menu js__content -->
+    </li>
+    <li>
+        <a class="waves-effect" href="/kontak"><i class="menu-icon fas fa-address-card"></i><span>Kontak</span></a>
+        <!-- /.sub-menu js__content -->
+    </li> 
+</ul>
