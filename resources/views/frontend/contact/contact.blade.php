4d86adf3f7698b5c59ffe1e616663a2eabf9eaff
herry88 <herry081288@gmail.com>
2020-03-29 12:51:10 +0700

    Done

---
+++ resources/views/frontend/contact/contact.blade.php
@@ -0,0 +1,85 @@
+<div class="w3-contact py-5" id="contact">
+    <div class="container py-lg-5">
+        <h3 class="stat-title text-center pb-5">contact us
+        </h3>
+        <div class="row contact-form pt-lg-5">
+            <!-- contact details -->
+            <div class="col-lg-6 contact-bottom d-flex flex-column contact-right-w3ls">
+                <h5>get in touch</h5>
+                <div class="fv3-contact">
+                    <div class="row">
+                        <div class="col-2">
+                            <span class="fas fa-envelope-open"></span>
+                        </div>
+                        <div class="col-10 contact-add">
+                            <h6>email</h6>
+                            <p>
+                                <a href="mailto:example@email.com" class="text-dark">info@example.com</a>
+                            </p>
+                        </div>
+                    </div>
+                </div>
+                <div class="fv3-contact my-4">
+                    <div class="row">
+                        <div class="col-2">
+                            <span class="fas fa-phone-volume"></span>
+                        </div>
+                        <div class="col-10 contact-add">
+                            <h6>phone</h6>
+                            <p>+456 123 7890</p>
+                        </div>
+                    </div>
+                </div>
+                <div class="fv3-contact">
+                    <div class="row">
+                        <div class="col-2">
+                            <span class="fas fa-home"></span>
+                        </div>
+                        <div class="col-10 contact-add">
+                            <h6>address</h6>
+                            <p>+90 nsequursu dsdesdc, Street State 34789.</p>
+                        </div>
+                    </div>
+                </div>
+            </div>
+            <div class="col-lg-6 wthree-form-left my-lg-0 mt-5">
+                <h5>send us a mail</h5>
+                <!-- contact form grid -->
+                <div class="contact-top1">
+                    <form action="#" method="get" class="contact-wthree">
+                        <div class="form-group d-flex">
+                            <label>
+                                Name
+                            </label>
+                            <input class="form-control" type="text" placeholder="Johnson" name="email" required="">
+                        </div>
+                        <div class="form-group d-flex">
+                            <label>
+                                Email
+                            </label>
+                            <input class="form-control" type="email" placeholder="example@email.com" name="email" required="">
+                        </div>
+                        <div class="form-group d-flex">
+                            <label>
+                                Phone
+                            </label>
+                            <input class="form-control" type="text" placeholder="XXXX XXXX XX" name="email" required="">
+                        </div>
+                        <div class="form-group d-flex">
+                            <label>
+                                Message
+                            </label>
+                            <textarea class="form-control" rows="5" id="contactcomment" placeholder="Your message" required></textarea>
+                        </div>
+                        <div class="d-flex justify-content-end">
+                            <button type="submit" class="btn btn-agile btn-block w-25">Send</button>
+                        </div>
+                    </form>
+                </div>
+                <!--  //contact form grid ends here -->
+            </div>
+
+        </div>
+        <!-- //contact details container -->
+    </div>
+</div>
